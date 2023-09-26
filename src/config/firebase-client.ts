dc5646c97a59f911dd6406c2b155262aaff42100
facumoyano <facumoyano44@gmail.com>
2023-09-26 14:22:24 -0300

    add auth with firebase

---
+++ src/config/firebase-client.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const apiKey = import.meta.env.PUBLIC_API_KEY;
+const authDomain = import.meta.env.PUBLIC_AUTH_DOMAIN;
+const projectId = import.meta.env.PUBLIC_PROJECT_ID;
+const storageBucket = import.meta.env.PUBLIC_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.PUBLIC_MESSAGING_SENDER_ID;
+const appId = import.meta.env.PUBLIC_APP_ID;
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+}
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/config/firebase-server.ts
@@ -0,0 +1,28 @@
+import type { ServiceAccount } from "firebase-admin";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import type {App} from 'firebase-admin/app';
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: import.meta.env.FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+  client_id: import.meta.env.FIREBASE_CLIENT_ID,
+  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+let app: App;
+
+if (!getApps().length) {
+  app = initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+  });
+} else {
+  app = getApps()[0]; // si ya está inicializado, úsalo
+}
+
+export { app };
