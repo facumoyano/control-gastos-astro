7ec983faf36a32630d38e75d75f04b4b192ebe35
facumoyano <facumoyano44@gmail.com>
2023-09-26 19:30:01 -0300

    add view expenses

---
+++ src/pages/api/expenses/[id]/index.ts
@@ -0,0 +1,57 @@
+import type { APIRoute } from "astro";
+import { app } from "../../../../config/firebase-server";
+import { getFirestore } from "firebase-admin/firestore";
+
+const db = getFirestore(app);
+const expensesRef = db.collection("expenses");
+
+export const POST: APIRoute = async ({ params, redirect, request }) => {
+  const formData = await request.formData();
+  const title = formData.get("title")?.toString();
+  const amount = formData.get("amount");
+  const date = formData.get("date");
+  const category = formData.get("category");
+
+  if (!title || !amount || !date || !category ) {
+    return new Response("Faltan campos obligatorios", {
+      status: 400,
+    });
+  }
+
+  if (!params.id) {
+    return new Response("No se puede encontrar el amigo", {
+      status: 404,
+    });
+  }
+
+  try {
+    await expensesRef.doc(params.id).update({
+        title,
+        amount,
+        date,
+        category
+    });
+  } catch (error) {
+    return new Response("Algo salió mal", {
+      status: 500,
+    });
+  }
+  return redirect("/");
+};
+
+export const DELETE: APIRoute = async ({ params, redirect }) => {
+  if (!params.id) {
+    return new Response("No se puede encontrar el amigo", {
+      status: 404,
+    });
+  }
+
+  try {
+    await expensesRef.doc(params.id).delete();
+  } catch (error) {
+    return new Response("Algo salió mal", {
+      status: 500,
+    });
+  }
+  return redirect("/");
+};
---
+++ src/pages/api/expenses/index.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from "astro";
+import { app } from "../../../config/firebase-server";
+import { getFirestore } from "firebase-admin/firestore";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const title = formData.get("title")?.toString();
+  const amount = formData.get("amount");
+  const date = formData.get("date");
+  const category = formData.get("category");
+
+  if (!title || !amount || !date || !category ) {
+    return new Response("Faltan campos obligatorios", {
+      status: 400,
+    });
+  }
+  
+  try {
+    const db = getFirestore(app);
+    const expensesRef = db.collection("expenses");
+    await expensesRef.add({
+      title,
+      amount: Number(amount),
+      date,
+      category,
+      createdAt: Date.now()
+
+    });
+  } catch (error) {
+    console.error(error)
+    return new Response("Algo salió mal", {
+      status: 500,
+    });
+  }
+  return redirect("/expenses");
+};
